d2f66e196f192977fe8a0b0608e4e1cf0667cffc
TwelveT <2471835953@qq.com>
2023-08-10 11:48:58 +0800

    插件化Gorm, Redis

--- plugin/redis/global/gloabl.go
+++
@@ -1,9 +0,0 @@
-package global
-
-import (
-	"github.com/twelvet-s/gins/plugin/redis/config"
-)
-
-var (
-	CONFIG *config.Redis // 配置
-)
--- plugin/redis/redis.go
+++ plugin/redis/redis.go
@@ -6,26 +6,26 @@ import (
 	"github.com/go-redis/redis/v8"
 	"github.com/twelvet-s/gins/framework/global"
 	"github.com/twelvet-s/gins/plugin/redis/config"
-	globalPlugin "github.com/twelvet-s/gins/plugin/redis/global"
 	"go.uber.org/zap"
 )
 
 var (
+	CONFIG   *config.Redis // 配置
 	INSTANCE *redis.Client // redis实例
 )
 
 type PluginRedis struct{}
 
 func CreateRedisPlug(redisConfig *config.Redis) *PluginRedis {
-	globalPlugin.CONFIG = redisConfig
+	CONFIG = redisConfig
 	return &PluginRedis{}
 }
 
 func (*PluginRedis) Register(group *gin.RouterGroup) {
 	client := redis.NewClient(&redis.Options{
-		Addr:     globalPlugin.CONFIG.Addr,
-		Password: globalPlugin.CONFIG.Password, // no password set
-		DB:       globalPlugin.CONFIG.DB,       // use default DB
+		Addr:     CONFIG.Addr,
+		Password: CONFIG.Password, // no password set
+		DB:       CONFIG.DB,       // use default DB
 	})
 	pong, err := client.Ping(context.Background()).Result()
 	if err != nil {
