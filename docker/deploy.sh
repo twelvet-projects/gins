31ae5d6c22602d91ac35adf7777b1926ff966cbf
TwelveT <2471835953@qq.com>
2023-08-10 16:27:40 +0800

    actions

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -6,18 +6,6 @@ usage() {
   exit 1
 }
 
-# 初始化
-init() {
-  # copy sql
-  echo "begin copy sql "
-  cp ../data/gins.sql ./mysql/db
-  cp ../data/gins_job.sql ./mysql/db
-
-  # copy jar
-  echo "begin copy gins-admin "
-  cp ../gins-admin/target/gins-admin.jar ./gins/gins-admin/jar
-}
-
 # 开启所需端口
 port() {
   firewall-cmd --add-port=8080/tcp --permanent
@@ -33,27 +21,7 @@ base() {
 
 # 启动程序模块（必须）
 server() {
-  docker-compose up -d gins-admin
-}
-
-# 启动nginx（必须）
-nginx() {
-  if [ ! -d "./twelvet-ui" ];then
-    rm -rf ./twelvet-ui
-
-    # 获取前端UI
-    git clone https://gitee.com/twelvet/twelvet-ui twelvet-ui
-
-    # 执行打包
-    cd ./twelvet-ui && yarn install && yarn run build
-
-    # 移动打包文件
-    \cp -rf ./dist/* ../nginx/html/
-  else
-    echo "前端已初始化"
-  fi
-
-  docker-compose up -d twelvet-nginx
+  docker-compose up -d gins
 }
 
 # 关闭所有环境/模块
@@ -68,9 +36,6 @@ rm() {
 
 # 根据输入参数，选择执行对应方法，不输入则执行使用说明
 case "$1" in
-"init")
-  init
-  ;;
 "port")
   port
   ;;
@@ -79,9 +44,6 @@ case "$1" in
   ;;
 "server")
   server
-  ;;
-"nginx")
-  nginx
   ;;
 "stop")
   stop
